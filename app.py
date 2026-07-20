4d211d53e95dbaf666f631cbdba27cf23ec33b49
Bhavana Dammalapati <bhavanadammalapati@gmail.com>
2026-07-20 09:22:22 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    gender = request.form['gender']
+    dob = request.form['dob']
+
+    return render_template('success.html', name=name, email=email)
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 5001)
