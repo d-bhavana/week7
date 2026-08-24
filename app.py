0e74f5f0dc168dbeee8be07cd97237942971c18d
d-bhavana <bhavanadammalapati@gmail.com>
2026-08-24 10:59:36 +0530

    Change app port from 5000 to 5001

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def register():
     return render_template('success.html', name=name, email=email)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port = 5000)
+    app.run(host='0.0.0.0',debug=True, port = 5001)
